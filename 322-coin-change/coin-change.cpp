b5317697ce8d0f133fdaa6addadd46aebbf27b73
Ashwan Shan <ashwanishan22@gmail.com>
2026-09-03 11:05:32 +0530

    Time: 31 ms (59.05%) | Memory: 13.6 MB (97.49%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,22 +2,13 @@ int dp[13][10001];
 int fun(int i,vector<int>& coins,int amount){
     if(amount == 0) return 0;
     if(i >= coins.size() || amount < 0){
-        return INT_MAX;
+        return 1e9;
     }
-    
-    
-     int take = INT_MAX;
-    if(dp[i][amount]!= -1) return dp[i][amount];
-    if(coins[i]<=amount){
-        
-       int x =  fun(i,coins,amount- coins[i]);
-       if(x!= INT_MAX){
-        take = 1+x;
-       }
-    }
-    
-    int nottake = fun(i+1,coins,amount);
-    return dp[i][amount] = min(take,nottake);
+    if(dp[i][amount] != -1) return dp[i][amount];
+    int take = 1+ fun(i,coins,amount-coins[i]);
+    int notTake = fun(i+1,coins,amount);
+
+    return dp[i][amount] = min(take,notTake);
 }
 class Solution {
 public:  
@@ -25,7 +16,7 @@ public:
         //vector<int> coins;
          memset(dp,-1,sizeof(dp));
          int ans = fun(0,coins,amount);
-         if(ans == INT_MAX) return -1;
+         if(ans==1e9) return -1;
          return ans;
     }
 };
