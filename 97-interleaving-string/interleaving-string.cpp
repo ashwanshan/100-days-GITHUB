381caf55e04467b14f866099c1b161a1c5133c35
Ashwan Shan <ashwanishan22@gmail.com>
2026-09-17 10:06:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (97.42%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,27 @@
+int dp[101][201];
+bool fun(int i,int j,string &s1,string &s2, string &s3){
+     int k=i+j;
+    if(k == s3.size()) return true;
+    bool ans = false;
+
+    if(dp[i][j] != -1)
+    return dp[i][j];
+    if(i<s1.size() && s1[i] == s3[k]){
+      ans = fun(i+1,j,s1,s2,s3);
+    }
+
+    if(j<s2.size() && s2[j] == s3[k]){
+      ans = ans || fun(i,j+1,s1,s2,s3);
+    }
+    return dp[i][j] =ans;
+}
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+     if(s1.size()+s2.size() != s3.size())
+     return false;
+     memset(dp,-1,sizeof(dp));
+       return fun(0,0,s1,s2,s3);
+    }
+};
