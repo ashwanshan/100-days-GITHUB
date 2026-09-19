08aeb88bc7c74a71a2facf4ddc7902c1f915f3ac
Ashwan Shan <ashwanishan22@gmail.com>
2026-09-19 13:08:36 +0530

    Time: 8 ms (74.83%) | Memory: 14.2 MB (77.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,30 +1,22 @@
-int dp[2501][2501];
-int fun(int i, int p,vector<int> &nums){
-    if(i>=nums.size()) return 0;
-    if(dp[i][p+1] != -1) return dp[i][p+1];
-   int ans =0;
-    if(p== -1){
-        int a= 1+ fun(i+1,i,nums);
-        int b=fun(i+1,p,nums);
-        ans=max(ans,a);
-        ans=max(ans,b);
-    }else{
-        if(nums[i]>nums[p]){
-        int a= 1+ fun(i+1,i,nums);
-        int b=fun(i+1,p,nums);   
-        ans=max(ans,a);
-        ans=max(ans,b);
-        }else{
-             int b=fun(i+1,p,nums);  
-              ans=max(ans,b);
-        }
-    }
-    return dp[i][p+1] =ans;
-}
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return fun(0,-1,nums);
+        vector<int>tmp;
+
+        for(auto a:nums){
+            if(tmp.size()==0 || a>tmp.back()){
+                tmp.push_back(a);
+            }else{
+                for(int j=0;j<tmp.size();j++){
+                    if(tmp[j]>=a){
+                        tmp[j]=a;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for(auto a:tmp) cout<<a<<" ";
+        return tmp.size();
     }
 };
