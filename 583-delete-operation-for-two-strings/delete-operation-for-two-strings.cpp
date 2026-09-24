362d0c8ee6830a0e3176924fed333632cccf20cf
Ashwan Shan <ashwanishan22@gmail.com>
2026-09-24 08:47:43 +0530

    Time: 91 ms (5.81%) | Memory: 14.2 MB (82.52%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+int dp[1001][1001];
+int fun(int i,int j,string &text1,string &text2){
+    if(i>=text1.size())  return text2.size()-j;
+    if(j>=text2.size()) return text1.size()-i;
+    if(dp[i][j] != -1)
+    return dp[i][j];
+     int ans=0;
+    if(text1[i]== text2[j]){
+         ans = fun(i+1,j+1,text1,text2);
+    }else{
+         int c1= 1 + fun(i+1,j,text1,text2);
+         int c2= 1 + fun(i,j+1,text1,text2);
+        //
+         ans=min(c1,c2);
+    }
+   return dp[i][j] = ans;
+}
+class Solution {
+public:
+    int minDistance(string text1, string text2) {
+        memset(dp,-1,sizeof(dp));
+        return fun(0,0,text1,text2);
+    }
+};
